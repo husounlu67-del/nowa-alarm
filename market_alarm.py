96fbb2c8a047fa0a03e27b07cef6dd239ba0f266
husounlu67-del <husounlu67@gmail.com>
2026-05-01 11:34:24 +0300

    Alarm guncellendi v20260501083456

--- market_alarm.py
+++ market_alarm.py
@@ -1,7 +1,7 @@
 """
 NOWA ONLINE - PAZAR ALARM SISTEMI (Termux / Telefon)
 =====================================================
-Versiyon : 20260501061546
+Versiyon : 20260501083456
 Calistir : python market_alarm.py
 Durdur   : Ctrl+C
 
@@ -16,7 +16,7 @@ from datetime import datetime
 # =============================================
 #  AYARLAR
 # =============================================
-VERSION          = "20260501061546"
+VERSION          = "20260501083456"
 GITHUB_RAW_URL   = "https://raw.githubusercontent.com/husounlu67-del/nowa-alarm/main/market_alarm.py"
 SCRIPT_PATH      = os.path.abspath(__file__)
 PCAP_PATH        = "/data/local/tmp/alarm_scan.pcap"
@@ -35,7 +35,7 @@ ALARM_LIST = [
     {"name": "Hope's Frozen Staff +4", "max_price": 6000000, "item_ids": ["347a450b"]},
     {"name": "Hope's Frozen Staff +5", "max_price": 10000000, "item_ids": ["357a450b"]},
     {"name": "Hope's Frozen Staff +6", "max_price": 80000000, "item_ids": ["367a450b"]},
-    {"name": "Hope's Frozen Staff +7", "max_price": 110000000, "item_ids": ["377a450b"]},
+    {"name": "Hope's Frozen Staff +7", "max_price": 11000000, "item_ids": ["377a450b"]},
     {"name": "Hope's Frozen Staff +8", "max_price": 220000000, "item_ids": ["387a450b"]},
     {"name": "Iceberg Staff +1", "max_price": 1500000, "item_ids": ["6dfb460b"]},
     {"name": "Iceberg Staff +2", "max_price": 1500000, "item_ids": ["6efb460b"]},
