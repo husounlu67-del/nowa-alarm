99cce5c151e302e14ebdc52e3c569c3d73578026
husounlu67-del <husounlu67@gmail.com>
2026-05-07 20:59:56 +0300

    Alarm guncellendi v20260507175955

--- market_alarm.py
+++ market_alarm.py
@@ -1,7 +1,7 @@
 """
 NOWA ONLINE - PAZAR ALARM SISTEMI (Termux / Telefon)
 =====================================================
-Versiyon : 20260505222652
+Versiyon : 20260507175955
 Calistir : python market_alarm.py
 Durdur   : Ctrl+C
 
@@ -16,7 +16,7 @@ from datetime import datetime
 # =============================================
 #  AYARLAR
 # =============================================
-VERSION          = "20260505222652"
+VERSION          = "20260507175955"
 GITHUB_RAW_URL   = "https://raw.githubusercontent.com/husounlu67-del/nowa-alarm/main/market_alarm.py"
 SCRIPT_PATH      = os.path.abspath(__file__)
 PCAP_PATH        = "/data/local/tmp/alarm_scan.pcap"
@@ -436,7 +436,7 @@ ALARM_LIST = [
     {"name": "Courage Pendant Old", "max_price": 8000000, "item_ids": ["460b6414"]},
     {"name": "Courage Pendant +1", "max_price": 220000000, "item_ids": ["dd7e1413"]},
     {"name": "Elderwood Belt Old", "max_price": 5000000, "item_ids": ["97de8b14"]},
-    {"name": "Elderwood Belt +0", "max_price": 220000000, "item_ids": ["013f4a14"]},
+    {"name": "Elderwood Belt +0", "max_price": 50000000, "item_ids": ["013f4a14"]},
     {"name": "Elderwood Belt +1", "max_price": 220000000, "item_ids": ["3f404a14"]},
     {"name": "Skull Belt Old", "max_price": 20000000, "item_ids": ["684b9014"]},
     {"name": "Belt of STR Old", "max_price": 2000000, "item_ids": ["58ef4b14"]},
